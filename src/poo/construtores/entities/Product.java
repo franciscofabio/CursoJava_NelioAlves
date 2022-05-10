256342209959aa0dd6afc5e1700da380448572e9
Francisco Fábio <franciscofabio18@hotmail.com>
2022-05-10 14:48:35 -0300

    Gerando construtor, getters e setters automaticamente

--- src/poo/construtores/entities/Product.java
+++ src/poo/construtores/entities/Product.java
@@ -20,28 +20,27 @@ public class Product {
 		this.name = name;
 		this.price = price;
 	}
-	
-	//geters e seters
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}

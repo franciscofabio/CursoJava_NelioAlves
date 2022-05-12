2b408de982d9e6b9525c9aaabada30112f86a348
Francisco Fábio <franciscofabio18@hotmail.com>
2022-05-11 22:52:08 -0300

    Minha solução para Exercicio de Fixação POO - Vetores

---
+++ src/poo/array/exercicioFixacao/application/Program.java
@@ -0,0 +1,46 @@
+package poo.array.exercicioFixacao.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import poo.array.exercicioFixacao.entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();
+		
+		Rent[] rents = new Rent[10];
+		
+		for (int i = 0; i < n; i++) {
+			sc.nextLine();
+			System.out.println();
+			System.out.printf("Rent #%d:%n",i+1);
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			
+			rents[room] = new Rent(name, email, room);
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms:");
+		
+		for (int i = 0; i < rents.length; i++) {
+			if (rents[i] != null) {
+				System.out.printf("%d: %s, %s%n",
+						rents[i].getRoom(), rents[i].getName(), rents[i].getEmail());
+			}	
+		}
+		
+		sc.close();
+	}
+}
